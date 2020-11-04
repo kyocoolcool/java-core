69252d12c5ed621a6a58cb569185fc719522d786
kyocoolcool <kyocoolcool@hotmail.com>
2020-11-05 00:28:07 +0800

    update

---
+++ src/main/java/core/v2ch02/buffer/BufferTest.java
@@ -0,0 +1,10 @@
+package core.v2ch02.buffer;
+
+import java.nio.ByteBuffer;
+
+public class BufferTest {
+    public static void main(String[] args) {
+        ByteBuffer buffer=ByteBuffer.allocate(10);
+
+    }
+}
